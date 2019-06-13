b2155f9211ac2d81adfe9cc11c6aeda2eb9909fa
liyishuuuu <lys7354@163.com>
2019-06-13 21:17:57 +0800

    设置tableView

--- Weibo/Class/View/Main/WBBaseViewController.swift
+++ Weibo/Class/View/Main/WBBaseViewController.swift
@@ -12,7 +12,8 @@ class WBBaseViewController: UIViewController {
 
     /// 自定义导航条
     lazy var navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 64))
-    
+    /// 定义tableView
+    var tableView: UITableView?
     /// 以后设置导航栏按钮使用 navItem
     lazy var navItem = UINavigationItem()
 
@@ -41,10 +42,19 @@ class WBBaseViewController: UIViewController {
 // MARK: - 设置界面
 extension WBBaseViewController {
     @objc func setUpUI() {
+       self.setupNavigation()
+        self.setupTableView()
+    }
+    
+    /// 设置tableView
+    private func setupTableView() {
+        let tableView: UITableView = UITableView(frame: view.bounds, style: .plain)
+        view.insertSubview(tableView, belowSubview: navigationBar)
+    }
+    /// 设置导航条
+    private func setupNavigation() {
         /// 添加导航条
         view.addSubview(navigationBar)
-        navItem.leftBarButtonItem = UIBarButtonItem(title: "注册", target: self, action: #selector(register))
-        navItem.rightBarButtonItem = UIBarButtonItem(title: "登录", target: self, action: #selector(login))
         navigationBar.items = [navItem]
         /// 设置navigationBar的字体颜色
         navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.darkGray,
@@ -52,4 +62,13 @@ extension WBBaseViewController {
         navigationBar.tintColor = UIColor.orange
         navigationBar.barTintColor = UIColor.white
     }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 5
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return
+//    }
+    
 }
