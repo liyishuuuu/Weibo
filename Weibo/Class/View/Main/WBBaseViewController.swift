48979abacdf5747b7657ae69067a8eb900a94299
liyishuuuu <lys7354@163.com>
2019-06-13 21:34:17 +0800

    隔离不同类的函数

--- Weibo/Class/View/Main/WBBaseViewController.swift
+++ Weibo/Class/View/Main/WBBaseViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
-
+///
+///
+/// 注意: extension 中不能有属性
+///      extension 中不能重写父类方法,是子类的职责，扩展时对类的拓展
+///
 class WBBaseViewController: UIViewController {
 
     /// 自定义导航条
     lazy var navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 64))
-    /// 定义tableView
+    /// 定义tableView, 如果用户没有登录就不创建
     var tableView: UITableView?
-    /// 以后设置导航栏按钮使用 navItem
+    /// 自定义导航条目，以后设置导航栏按钮使用 navItem
     lazy var navItem = UINavigationItem()
 
     // MARK - override
@@ -62,13 +66,20 @@ extension WBBaseViewController {
         navigationBar.tintColor = UIColor.orange
         navigationBar.barTintColor = UIColor.white
     }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension WBBaseViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 5
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        return
-//    }
+    //基类只是准备方法，自雷负责具体的实现
+    // 子类的数据源不需要 super
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 只是保证没有语法错误
+        return UITableViewCell()
+    }
+    
     
 }
