928979b59e42ec9e8e0233ca998f5eeb2c913bab
liyishuuuu <lys7354@163.com>
2019-06-27 07:46:12 +0800

    调整格式

--- Weibo/Tools/WBCommon.swift
+++ Weibo/Tools/WBCommon.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 // MARK - 应用程序信息
+
 let WBAppKey = "1236293576"
 let WBAppSecret = "1206617644d54af36f91faaa8fe552d4"
 let WBRedirectUri = "https://baidu.com"
 
 // MARK - 全局通知定义
+
 // 用户需要登录通知
 let WBUsershouldLoginNotification = "WBUsershouldLoginNotification"
