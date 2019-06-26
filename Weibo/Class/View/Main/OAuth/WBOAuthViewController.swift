044dc5072d2ec4f9f6c8f64d05f2eabdb876b85a
liyishuuuu <lys7354@163.com>
2019-06-27 07:45:31 +0800

    调整格式

--- Weibo/Class/View/Main/OAuth/WBOAuthViewController.swift
+++ Weibo/Class/View/Main/OAuth/WBOAuthViewController.swift
@@ -16,24 +16,30 @@ class WBOAuthViewController: UIViewController {
     override func loadView() {
         view = webView
         view.backgroundColor = UIColor.white
-        
+
         // 设置导航栏
         title = "登录新浪微博"
 
         // 导航栏按钮
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回", target: self, action: #selector(close), isBack: true)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "返回",
+                                                           target: self,
+                                                           action: #selector(close),
+                                                           isBack: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 加载授权页面
         let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(WBAppKey)&redirect_uri=\(WBRedirectUri)"
+        
         // 建立要访问的url资源
         guard let url = URL(string: urlString) else {
             return
         }
+
         // 建立请求
         let request = URLRequest(url: url)
+
         // 加载请求
         webView.loadRequest(request)
     }
