ee480605454fff115764676a419b378a8bdd2037
liyishuuuu <lys7354@163.com>
2019-07-06 16:56:08 +0800

    准备欢迎视图和新特性视图

---
+++ Weibo/Class/View/Main/NewFeature/WBNewFeatureView.swift
@@ -0,0 +1,22 @@
+//
+//  WBNewFeatureView.swift
+//  Weibo
+//
+//  Created by liyishu on 2019/7/6.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+/// 新特性视图
+class WBNewFeatureView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Weibo/Class/View/Main/NewFeature/WBWelcomeView.swift
@@ -0,0 +1,22 @@
+//
+//  WBWelcome.swift
+//  Weibo
+//
+//  Created by liyishu on 2019/7/6.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+// 欢迎视图
+class WBWelcomeView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
