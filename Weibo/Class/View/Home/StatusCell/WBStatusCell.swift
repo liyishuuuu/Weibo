7d6ef35d6e35d5e7e78a1c847ef280f704cd124d
liyishuuuu <lys7354@163.com>
2019-07-10 22:25:18 +0800

    微博控件连线

--- Weibo/Class/View/Home/StatusCell/WBStatusCell.swift
+++ Weibo/Class/View/Home/StatusCell/WBStatusCell.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class WBStatusCell: UITableViewCell {
 
+    // MARK: - IBOutlet
+    
+    /** 头像 */
+    @IBOutlet weak var iconView: UIImageView!
+    /** 姓名 */
+    @IBOutlet weak var nameLabel: UILabel!
+    /** 会员图标 */
+    @IBOutlet weak var memberIconView: UIImageView!
+    /** 时间 */
+    @IBOutlet weak var timeLabel: UILabel!
+    /** 来源 */
+    @IBOutlet weak var sourceLabel: UILabel!
+    /** vip图片 */
+    @IBOutlet weak var vipIconView: UIImageView!
+    /** 微博内容 */
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    // MARK： - override method
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
