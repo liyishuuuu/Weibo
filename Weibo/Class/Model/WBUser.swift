fbed7c26cf15e77220ab64236f0ac4b323ad6cee
liyishuuuu <lys7354@163.com>
2019-07-13 10:25:48 +0800

    添加注释

--- Weibo/Class/Model/WBUser.swift
+++ Weibo/Class/Model/WBUser.swift
@@ -10,10 +10,20 @@ import UIKit
 
 /// 微博用户模型
 class WBUser: NSObject {
+
+    /** 用户ID */
     var id: Int64 = 0
+
+    /** 用户昵称 */
     var screen_name: String?
+
+    /** 用户头像地址（中图），50×50像素 */
     var profile_image_url: String?
+
+    /** 微博认证用户 */
     var verified_type: Int = 0
+
+    /** 会员等级 */
     var mbrank: Int = 0
 
     override var description: String {
