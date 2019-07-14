441cc31d1920bb38f9059b5bbf714dcf6f332aa1
liyishuuuu <lys7354@163.com>
2019-07-14 13:17:27 +0800

    九宫格布局

--- Weibo/Class/View/Home/StatusCell/WBStatusPictureView.swift
+++ Weibo/Class/View/Home/StatusCell/WBStatusPictureView.swift
@@ -10,4 +10,34 @@ import UIKit
 
 class WBStatusPictureView: UIView {
     @IBOutlet weak var heightCons: NSLayoutConstraint!
+    override func awakeFromNib() {
+        setupUI()
+    }
+}
+
+// MARK: - 设置界面
+extension WBStatusPictureView {
+    private func setupUI() {
+        clipsToBounds = true
+        let count = 3
+        let rect = CGRect(x: 0,
+                          y: WBStatusPictureViewOutterMargin,
+                          width: WBStatusPictureItemWidth,
+                          height: WBStatusPictureItemWidth)
+        for i in 0..<count * count {
+            
+            let iv = UIImageView()
+            iv.backgroundColor = UIColor.red
+
+            // 行
+            let row = CGFloat(i / count)
+            // 列
+            let col = CGFloat(i % count)
+
+            let xOffset = col*(WBStatusPictureViewInnerMargin + WBStatusPictureItemWidth)
+            let yOffset = row*(WBStatusPictureViewInnerMargin + WBStatusPictureItemWidth)
+            iv.frame = rect.offsetBy(dx: xOffset, dy: yOffset)
+            addSubview(iv)
+        }
+    }
 }
